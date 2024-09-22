463be677e674274c0c5bb0fbdda934d9982a029f
Nathan Li <moncskoli@gmail.com>
2024-09-22 16:15:01 -0400

    allow pinging by cronjob to keep server awake

--- backend/app.py
+++ backend/app.py
@@ -5,6 +5,10 @@ from test_model import test_model
 app = Flask(__name__)
 CORS(app)
 
+@app.route('/')
+def health_check():
+    return "Server is running", 200
+
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json()
